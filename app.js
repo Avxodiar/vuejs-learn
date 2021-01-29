24b0d1063d5fb89fbe20127ebe761d36618d38ff
KHaritov Andrei <avxodiar@gmail.com>
2021-01-29 14:08:41 +0300

    Создание простейшего todo-list'а

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const App = {
   data() {
     return {
-      counter: 0
+      counter: 0,
+      title: 'ToDO List',
+      titleString: 'Опишите задачу',
+      inputValue: '',
+      todoList: ['Составить план', 'Добавить 2 задачи']
+    }
+  },
+  methods: {
+    inputChangeHandler(evt) {
+      this.inputValue = evt.target.value
+    },
+    addTask() {
+      let task = this.inputValue;
+      if (task != '') {
+        this.todoList.push(task)
+        this.inputValue = ''
+      }
     }
   }
 }
